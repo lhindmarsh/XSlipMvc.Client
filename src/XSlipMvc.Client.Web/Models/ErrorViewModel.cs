b5404e1702adf0b3754e10dfd4cfbe58471e6d91
Lee Hindmarsh <lee@ljhmedia.com>
2025-10-02 13:35:07 +0100

    Initial mvc Web App creation

---
+++ src/XSlipMvc.Client.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace XSlipMvc.Client.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
