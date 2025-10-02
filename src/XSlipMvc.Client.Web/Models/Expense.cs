acf0c3369769bad981f5c9ae8c2c356eb7e23a8e
Lee Hindmarsh <lee@ljhmedia.com>
2025-10-02 15:32:30 +0100

    New model for Expense, and JsonPropertyName decorators

---
+++ src/XSlipMvc.Client.Web/Models/Expense.cs
@@ -0,0 +1,19 @@
+﻿namespace XSlipMvc.Client.Web.Models
+{
+    using System.Text.Json.Serialization;
+
+    public class Expense
+    {
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+
+        [JsonPropertyName("description")]
+        public string Description { get; set; } = null!;
+
+        [JsonPropertyName("amount")]
+        public decimal Amount { get; set; }
+
+        [JsonPropertyName("date")]
+        public string Category { get; set; } = null!;
+    }
+}
