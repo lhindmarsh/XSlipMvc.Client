7aed1ccd2d1113704dc525bca18f9c0a06be96c3
Lee Hindmarsh <lee@ljhmedia.com>
2025-10-03 12:20:02 +0100

    Updated DbContext DI with SQL server connection string

---
+++ src/XSlipMvc.Client.Infrastructure/Persistence/Configurations/DependencyInjection.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+using XSlipMvc.Client.Infrastructure.Persistence.Context;
+
+namespace XSlipMvc.Client.Infrastructure.Persistence.Configurations
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddDbContext<XSlipContext>(options =>
+                options.UseSqlServer(config.GetConnectionString("ConnectionString_Local")));
+
+            return services;
+        }
+    }
+}
--- src/XSlipMvc.Client.Web/Program.cs
+++ src/XSlipMvc.Client.Web/Program.cs
@@ -1,5 +1,16 @@
+using XSlipMvc.Client.Infrastructure.Persistence.Configurations;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container
+builder.Services.AddControllersWithViews();
+
+//Inject DbContext with SQLServer connectionstring
+builder.Services.AddInfrastructure(builder.Configuration);
+
 var app = builder.Build();
+
+app.UseHttpsRedirection();
 
 app.MapGet("/", () => "Hello World!");
 
