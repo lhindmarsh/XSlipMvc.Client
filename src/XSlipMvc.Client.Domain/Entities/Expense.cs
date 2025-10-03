8d4090593cd8b5702d77cbdfa9cd352c45c5bf8f
Lee Hindmarsh <lee@ljhmedia.com>
2025-10-03 10:42:25 +0100

    Add Domain project with first entity (model) and DbContext

--- src/XSlipMvc.Client.Domain/Entities/Expense.cs
+++ src/XSlipMvc.Client.Domain/Entities/Expense.cs
@@ -1,19 +1,26 @@
-﻿namespace XSlipMvc.Client.Web.Models
-{
-    using System.Text.Json.Serialization;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
+namespace XSlipMvc.Client.Domain.Entities
+{
     public class Expense
     {
         [JsonPropertyName("id")]
         public int Id { get; set; }
 
+        [Required]
         [JsonPropertyName("description")]
         public string Description { get; set; } = null!;
 
+        [Required]
+        [Range(typeof(decimal), "1.00", "5000", ErrorMessage = "Amount minimum is 1.00")]
         [JsonPropertyName("amount")]
         public decimal Amount { get; set; }
 
+        [Required]
         [JsonPropertyName("date")]
         public string Category { get; set; } = null!;
+
+        public DateTime Date { get; set; } = DateTime.Now;
     }
 }
---
+++ src/XSlipMvc.Client.Infrastructure/Persistence/Context/XSlipContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using XSlipMvc.Client.Domain.Entities;
+
+namespace XSlipMvc.Client.Infrastructure.Persistence.Context
+{
+    public class XSlipContext : DbContext
+    {
+        //Server Name=(localdb)\MSSQLLocalDB
+        //ConnectionString=Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=development-XSlip;Integrated Security=True;Trust Server Certificate=True
+
+        public XSlipContext(DbContextOptions<XSlipContext> options) : base(options)
+        { }
+
+        DbSet<Expense> Expenses { get; set; }
+    }
+}
