e81a37d446fff1b17b01f2395685740622f80270
Lee Hindmarsh <lee@ljhmedia.com>
2025-10-22 11:57:43 +0100

    Added infrastructure for ASP.NET Core Identity

---
+++ src/XSlipMvc.Client.Infrastructure/Identity/ApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace XSlipMvc.Client.Infrastructure.Identity
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string? DisplayName { get; set; }
+    }
+}
--- src/XSlipMvc.Client.Infrastructure/Persistence/Configurations/DependencyInjection.cs
+++ src/XSlipMvc.Client.Infrastructure/Persistence/Configurations/DependencyInjection.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 using XSlipMvc.Client.Application.Interfaces;
 using XSlipMvc.Client.Application.Services;
+using XSlipMvc.Client.Infrastructure.Identity;
 using XSlipMvc.Client.Infrastructure.Persistence.Context;
 using XSlipMvc.Client.Infrastructure.Persistence.Repositories;
 using XSlipMvc.Client.Infrastructure.Persistence.Seeding;
@@ -17,10 +19,32 @@ namespace XSlipMvc.Client.Infrastructure.Persistence.Configurations
     {
         public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration config)
         {
+            var connection = config.GetConnectionString("ConnectionString_SQLExpress");
+
+            //DbContext for SQL Server (Express)
             services.AddDbContext<XSlipContext>(options =>
-                options.UseSqlServer(config.GetConnectionString("ConnectionString_SQLExpress"))
-                .EnableSensitiveDataLogging()
+                options.UseSqlServer(connection)
                 .LogTo(Console.WriteLine, LogLevel.Information));
+
+            //DbContext for Identity (same database)
+            services.AddDbContext<ApplicationIdentityDbContext>(options =>
+                options.UseSqlServer(connection));
+
+            //Identity Registration
+            services.AddIdentity<ApplicationUser, IdentityRole>(options =>
+            {
+                options.Password.RequiredLength = 10;
+                options.Password.RequireDigit = true;
+                options.Password.RequireLowercase = true;
+                options.Password.RequireUppercase = true;
+                options.Password.RequireNonAlphanumeric = true;
+                options.Lockout.MaxFailedAccessAttempts = 5;
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+                options.User.RequireUniqueEmail = true;
+                options.SignIn.RequireConfirmedEmail = true;
+            })
+            .AddEntityFrameworkStores<ApplicationIdentityDbContext>()
+            .AddDefaultTokenProviders();
 
             services = AddServicesAndRepos(services);
 
